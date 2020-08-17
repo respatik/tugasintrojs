ff49864a120f212d1368242656de3ffd0735b5db
respatik <respatik@gmail.com>
2020-08-17 16:42:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// soal nomor 3 console.log(nama)
+console.log("Respati Kusumawardhani");
+// soal nomor 3 console.log(asal daerah)
+console.log(Jakarta);
+// soal nomor 3 console.log(tanggal lahir)
+console.log("04 Juni 1992");
+// soal nomor 3 console.log(umur)
+console.log(28);
+// soal nomor 3 console.log(posisipekerjaan)
+console.log(guru);
+/*soal nomor 4 (membuat komentar untuk fitur soal nomor 2)
+(soal nomor 2) mencoba membuat fitur prompt, confirm, alert*/
+namaUser = prompt("Nama User");
+asalNegara = prompt("Asal Negara");
+confirm("apakah data yang diisi sudah benar?");
+alert("Terima kasih sudah mengisi form");
+
+//soal nomer 5
+let biodata = {
+    nama: "Respati Kusumawardhani",
+    asaldaerah: "Jakarta",
+    tanggallahir: "4 Juni 1992",
+    umur: 28,
+    posisipekerjaan: "guru"
+}
+console.log(biodata);
+
+// soal nomer 6
+
+let sum = (25+10)
+console.log(sum);
+
+let times = (100*2)
+console.log(times);
+
+let divide = (99/2)
+console.log(divide)
+
+let mod = (99%2)
+console.log(mod)
